95c0d1a65b2957d74a22c078ef1310a3c67e5899
aransena <aransena@gmail.com>
2017-10-03 14:58:44 +0100

    jacobian reader

--- src/joint_state_publisher.py
+++ src/joint_state_publisher.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     global pub
     rospy.init_node('joint_state_node')
     sub = rospy.Subscriber('/robot/joint_states', JointState, cb)
-    pub = rospy.Publisher('/joint_states', JointState)
+    pub = rospy.Publisher('/joint_states', JointState, queue_size=-1)
+    rospy.loginfo('Publishing /robot/joint_states to /joint_states')
 
     rospy.spin()
 
--- src/moveit_jacobian.cpp
+++ src/moveit_jacobian.cpp
@@ -10,33 +10,43 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_state/conversions.h>
 
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_interface/planning_interface.h>
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "move_group_tutorial");
+    ros::init(argc, argv, "sawyer_jacobian");
     ros::NodeHandle n;
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
-    ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
-    robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
-    kinematic_state->setToDefaultValues();
-    const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("right_arm");
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-    const std::vector<std::string> &joint_names = joint_model_group->getJointModelNames();
+    static const std::string PLANNING_GROUP = "right_arm";
+    move_group_interface::MoveGroup move_group(PLANNING_GROUP);
 
-    std::vector<double> joint_values;
-    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-    for(std::size_t i = 0; i < joint_names.size(); ++i)
-    {
-        ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-    }
+    const robot_state::JointModelGroup *joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+    moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+
+    std::vector<double> joint_group_positions;
 
     Eigen::Vector3d reference_point_position(0.0,0.0,0.0);
     Eigen::MatrixXd jacobian;
-    kinematic_state->getJacobian(joint_model_group, kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
-                                 reference_point_position,
-                                 jacobian);
-    ROS_INFO_STREAM("Jacobian: " << jacobian);
 
-    ros::spinOnce();
+    ros::Rate rate(50);
+    while (n.ok()) {
+        current_state = move_group.getCurrentState();
+        jacobian = current_state->getJacobian(joint_model_group, reference_point_position);
+        ROS_INFO_STREAM("Jacobian: \n" << jacobian);
+        rate.sleep();
+    }
+
+//    const std::vector<std::string> &joint_names = joint_model_group->getActiveJointModelNames();
+//
+//    for(std::size_t i = 0; i < joint_names.size(); ++i)
+//    {
+//        ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_group_positions[i]);//joint_values[i]);
+//    }
+
+
 }
 
